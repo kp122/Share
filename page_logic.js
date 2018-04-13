610c57347f1377ef1ee8d9c31a6dda12604ade80
kunal <kunal.patel@elevenrobots.com>
2018-04-13 20:42:52 +0100

    update form from Bootstrap 3 to 4

--- page_logic.js
+++ page_logic.js
@@ -7,18 +7,33 @@
             onAddIngredientClk.counter = 2;
         }
 
-        document.getElementById("IngredientGroup").innerHTML += "<div id=\"" + onAddIngredientClk.counter + "\">" + 
-        "<input id=\"Ingredient1\" name=\"Ingredient1\" type=\"text\" placeholder=\"Ingredient + Quantity\" class=\"form-control input-md\" required=\"\">"+
+        
+        var bla = $('#Ingredient1').val();
+        var vals = [];
+
+        for (i = 1; i <= onAddIngredientClk.counter; i++) { 
+            vals.push( $("#Ingredient"+i).val() );
+        }
+
+    
+        
+        document.getElementById("IngredientGroup").innerHTML = document.getElementById("IngredientGroup").innerHTML + "<div id=\"" + onAddIngredientClk.counter + "\">" + 
+        "<input id=\"Ingredient" + onAddIngredientClk.counter +  "\" name=\"Ingredient"+onAddIngredientClk.counter+"\" type=\"text\" placeholder=\"Ingredient + Quantity\" class=\"form-control input-md\" required=\"\">"+
         "<button onclick=\"onRemoveTextBox('" + onAddIngredientClk.counter +"')\" type=\"button\" class=\"close\" aria-label=\"Close\">"+
         "<span aria-hidden=\"true\">&times;</span>"+
         "</button></div>";
-       
+    
+       for (i = 1; i <= onAddIngredientClk.counter; i++) { 
+            $("#Ingredient"+i).val(vals[i-1]);
+        }
+
         onAddIngredientClk.counter++;
     }
     function onRemoveTextBox(id)
     {
         var element = document.getElementById(id);
         element.outerHTML = "";
+        onAddIngredientClk.counter--;
         delete element;
     }
 }
