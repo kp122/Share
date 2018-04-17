ce0cf2cde398b8ac7545debc1962403e8e4b583d
kunal <kunal.patel@elevenrobots.com>
2018-04-17 20:45:19 +0100

    began dynamic instructions

--- app.js
+++ app.js
@@ -1,62 +1,100 @@
 
-async function GetData()
-{
+async function GetData() {
     console.log("Get Data Called");
-    
+
     console.log("Got here");
     const response = await fetch("http://127.0.0.1:3000/winter", {
         method: "GET",
-        headers: { "Content-Type": "text/plain"}}
+        headers: { "Content-Type": "text/plain" }
+    }
     );
     const json = await response.json();
-    
+
     console.log(json);
-    document.getElementById("Main").innerHTML = JSON.stringify(json); 
-    
-    
+    document.getElementById("Main").innerHTML = JSON.stringify(json);
+
+
 }
 
 window.addEventListener('load', e => {
-    console.log("Page loadeds");
+    console.log("Page loaded");
     //GetData();
-   
+
 })
 
 
-function onAddIngredientClk()
-    {
-
-        if ( typeof onAddIngredientClk.counter == 'undefined' ) {
-            // It has not... perform the initialization
-            onAddIngredientClk.counter = 2;
-        }
-
-        
-        var bla = $('#Ingredient1').val();
-        var vals = [];
-
-        for (i = 1; i <= onAddIngredientClk.counter; i++) { 
-            vals.push( $("#Ingredient"+i).val() );
-        }
- 
+function onAddInstructionClk(){
+    if (typeof onAddInstructionClk.counter == 'undefined') {
+        // It has not... perform the initialization
+        onAddInstructionClk.counter = 1;
+    }
     
-        
-        document.getElementById("IngredientGroup").innerHTML = document.getElementById("IngredientGroup").innerHTML + "<div id=\"" + onAddIngredientClk.counter + "\">" + 
-        "<input id=\"Ingredient" + onAddIngredientClk.counter +  "\" name=\"Ingredient"+onAddIngredientClk.counter+"\" type=\"text\" placeholder=\"Ingredient + Quantity\" class=\"form-control input-md\" required=\"\">"+
-        "<button onclick=\"onRemoveTextBox('" + onAddIngredientClk.counter +"')\" type=\"button\" class=\"close\" aria-label=\"Close\">"+
-        "<span aria-hidden=\"true\">&times;</span>"+
+    var counter = onAddInstructionClk.counter;
+
+    var vals = [];
+
+    //save the imput box values
+    for (i = 0; i < counter; i++) {
+        vals.push($("#Instruction" + i).val());
+    }
+
+    document.getElementById("InstructionGroup").innerHTML +=  "<div id=\"ins"+counter+"\">"+
+              "<input id=\"Instruction"+counter+"\" name=\"Instruction0Name\" type=\"text\" placeholder=\"Instruction\" class=\"form-control input-md\" required>"+
+              "<button type=\"button\" class=\"close\" aria-label=\"Close\" onclick=\"onRemoveInstruction('ins"+counter+"')\">"+
+              "<span aria-hidden=\"true\">&times;</span>"+
+              "</button></div>"
+
+    //restore the input box values
+    for (i = 0; i < counter; i++) {
+        $("#Instruction" + i).val(vals[i]);
+    }
+
+    onAddInstructionClk.counter++;
+
+}
+
+function onAddIngredientClk() {
+
+    if (typeof onAddIngredientClk.counter == 'undefined') {
+        // It has not... perform the initialization
+        onAddIngredientClk.counter = 1;
+    }
+
+    var vals = [];
+
+    //save the imput box values
+    for (i = 0; i < onAddIngredientClk.counter; i++) {
+        vals.push($("#Ingredient" + i).val());
+    }
+
+    //add a new input box 
+    document.getElementById("IngredientGroup").innerHTML = document.getElementById("IngredientGroup").innerHTML + "<div id=\"" + onAddIngredientClk.counter + "\">" +
+        "<input id=\"Ingredient" + onAddIngredientClk.counter + "\" name=\"Ingredient" + onAddIngredientClk.counter + "\" type=\"text\" placeholder=\"Ingredient + Quantity\" class=\"form-control input-md\" required=\"\">" +
+        "<button onclick=\"onRemoveTextBox('" + onAddIngredientClk.counter + "')\" type=\"button\" class=\"close\" aria-label=\"Close\">" +
+        "<span aria-hidden=\"true\">&times;</span>" +
         "</button></div>";
-    
-       for (i = 1; i <= onAddIngredientClk.counter; i++) { 
-            $("#Ingredient"+i).val(vals[i-1]);
-        }
 
-        onAddIngredientClk.counter++;
+    //restore the input box values
+    for (i = 0; i < onAddIngredientClk.counter; i++) {
+        $("#Ingredient" + i).val(vals[i]);
     }
-    function onRemoveTextBox(id)
-    {
-        var element = document.getElementById(id);
-        element.outerHTML = "";
-        onAddIngredientClk.counter--;
-        delete element;
-    }
+
+    onAddIngredientClk.counter++;
+}
+
+
+function onRemoveInstruction(id) {
+    var element = document.getElementById(id);
+    element.outerHTML = "";
+    onAddInstructionClk.counter--;
+ 
+    delete element;
+}
+
+function onRemoveTextBox(id) {
+    var element = document.getElementById(id);
+    element.outerHTML = "";
+    onAddIngredientClk.counter--;
+ 
+    delete element;
+}
